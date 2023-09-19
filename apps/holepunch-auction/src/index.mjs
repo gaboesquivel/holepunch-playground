efa1d1ed3f331cbf99e98fc37d935ff443b33111
Gabo Esquivel <contact@gaboesquivel.com>
2023-09-19 09:12:31 -0600

    wip: log client name

--- apps/holepunch-auction/src/index.mjs
+++ apps/holepunch-auction/src/index.mjs
@@ -5,7 +5,7 @@ import { config } from './config.mjs'
 
 async function main() {
   // Join the auctions topic
-  console.log('joining swarm topic ...')
+  console.log(`${config.clientName} joining swarm topic ...`)
   const swarm = new hyperswarm()
   const topic = Buffer.alloc(32).fill(config.topicKey) // A topic must be 32 bytes
   const discovery = swarm.join(topic, {
@@ -14,12 +14,13 @@ async function main() {
   })
   console.log('wait for for the topic to be fully announced on the dht ...')
   await discovery.flushed()
-  console.log('ready !')
+  console.log('ready for connection!')
 
   // On connection run simulation once
   let execAuction = false
   swarm.on('connection', async (socket, info) => {
     console.log('new connection!') // JSON.stringify(info)
+    // console.log('swarm peers', swarm.peers.keys())
 
     if (!execAuction) {
       await runScenario(socket, config.clientName)
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,10 @@ module.exports = {
       script: './apps/holepunch-auction/src/index.mjs',
       instances: 4,
       exec_mode: 'cluster',
+      watch: ['apps/holepuch-acutions'],
+      // Delay between restart
+      watch_delay: 1000,
+      ignore_watch: ['node_modules', 'node-data', '\\.git', '*.log'],
     },
   ],
 }
